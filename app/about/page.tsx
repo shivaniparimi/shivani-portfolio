260d0e6d70f15edf8cd2bacfd51a55436f975422
Shivani Parimi <shivaniparimi0@gmail.com>
2026-09-14 14:37:40 -0700

    text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -105,9 +105,6 @@ export default function About() {
             />
           </div>
           <div className="flex-1 space-y-4 text-left font-mono min-w-0">
-            <p className="text-xl sm:text-2xl font-semibold text-neutral-900 dark:text-white">
-              hi! i&apos;m shivani.
-            </p>
             <p className="text-base leading-relaxed text-neutral-800 dark:text-neutral-200">
               I&apos;m currently studying Math &amp; Computer Science at UC
               San Diego, hoping to pursue a full time career in software
